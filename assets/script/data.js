3b588018efc10b00451b45222e1988b366478192
Hiro Setsuya <adrianvincenthjavillo@iskolarngbayan.pup.edu.ph>
2025-11-21 09:42:45 +0800

    Compile the data that will use in the index.html

---
+++ assets/script/data.js
@@ -0,0 +1,26 @@
+var classes = [
+  {
+    name: "All Class",
+    image: "assets/img/all_class.png",
+    description:
+      "Explore all the diverse job classes available in Ragnarok Online, each with unique skills and abilities.",
+  },
+  {
+    name: "Swordsman",
+    image: "assets/img/swordsman.png",
+    description:
+      "The Swordsman is a melee combatant known for their strength and defense, excelling in close-range battles.",
+  },
+  {
+    name: "Mage",
+    image: "assets/img/mage.png",
+    description:
+      "The Mage wields powerful magic spells to attack enemies from a distance and support allies with elemental forces.",
+  },
+  {
+    name: "Archer",
+    image: "assets/img/archer.png",
+    description:
+      "The Archer specializes in ranged attacks using bows and arrows, striking foes from afar with precision.",
+  },
+];
