8c00ad87172bfa54a8dc09dfcd7a4915d258239d
Hiro Setsuya <adrianvincenthjavillo@iskolarngbayan.pup.edu.ph>
2025-11-21 10:13:05 +0800

    Add the classes will use

--- assets/script/data.js
+++ assets/script/data.js
@@ -1,26 +1,255 @@
 var classes = [
   {
-    name: "All Class",
-    image: "assets/img/all_class.png",
-    description:
-      "Explore all the diverse job classes available in Ragnarok Online, each with unique skills and abilities.",
+    name: "Merchant",
+    image:
+      "https://p16-marketing-sg.bytedgame.com/obj/g-marketing-assets-sg/static/media/job1600.0b54c53f.png",
+    description: "Description for Merchant.",
   },
   {
-    name: "Swordsman",
-    image: "assets/img/swordsman.png",
-    description:
-      "The Swordsman is a melee combatant known for their strength and defense, excelling in close-range battles.",
+    name: "Blacksmith",
+    image: "assets/img/blacksmith.png",
+    description: "Description for Blacksmith.",
   },
+  {
+    name: "Whitesmith",
+    image: "assets/img/whitesmith.png",
+    description: "Description for Whitesmith.",
+  },
+  {
+    name: "Mechanic",
+    image: "assets/img/mechanic.png",
+    description: "Description for Mechanic.",
+  },
+  {
+    name: "Alchemist",
+    image: "assets/img/alchemist.png",
+    description: "Description for Alchemist.",
+  },
+  {
+    name: "Creator",
+    image: "assets/img/creator.png",
+    description: "Description for Creator.",
+  },
+  {
+    name: "Genetic",
+    image: "assets/img/genetic.png",
+    description: "Description for Genetic.",
+  },
+
   {
     name: "Mage",
     image: "assets/img/mage.png",
-    description:
-      "The Mage wields powerful magic spells to attack enemies from a distance and support allies with elemental forces.",
+    description: "Description for Mage.",
   },
+  {
+    name: "Wizard",
+    image: "assets/img/wizard.png",
+    description: "Description for Wizard.",
+  },
+  {
+    name: "High Wizard",
+    image: "assets/img/high_wizard.png",
+    description: "Description for High Wizard.",
+  },
+  {
+    name: "Warlock",
+    image: "assets/img/warlock.png",
+    description: "Description for Warlock.",
+  },
+
+  {
+    name: "Sage",
+    image: "assets/img/sage.png",
+    description: "Description for Sage.",
+  },
+  {
+    name: "Professor",
+    image: "assets/img/professor.png",
+    description: "Description for Professor.",
+  },
+  {
+    name: "Sorcerer",
+    image: "assets/img/sorcerer.png",
+    description: "Description for Sorcerer.",
+  },
+
+  {
+    name: "Acolyte",
+    image: "assets/img/acolyte.png",
+    description: "Description for Acolyte.",
+  },
+  {
+    name: "Priest",
+    image: "assets/img/priest.png",
+    description: "Description for Priest.",
+  },
+  {
+    name: "High Priest",
+    image: "assets/img/high_priest.png",
+    description: "Description for High Priest.",
+  },
+  {
+    name: "Archbishop",
+    image: "assets/img/archbishop.png",
+    description: "Description for Archbishop.",
+  },
+
+  {
+    name: "Monk",
+    image: "assets/img/monk.png",
+    description: "Description for Monk.",
+  },
+  {
+    name: "Champion",
+    image: "assets/img/champion.png",
+    description: "Description for Champion.",
+  },
+  {
+    name: "Shura",
+    image: "assets/img/shura.png",
+    description: "Description for Shura.",
+  },
+
+  {
+    name: "Swordsman",
+    image: "assets/img/swordsman.png",
+    description: "Description for Swordsman.",
+  },
+  {
+    name: "Knight",
+    image: "assets/img/knight.png",
+    description: "Description for Knight.",
+  },
+  {
+    name: "Lord Knight",
+    image: "assets/img/lord_knight.png",
+    description: "Description for Lord Knight.",
+  },
+  {
+    name: "Rune Knight",
+    image: "assets/img/rune_knight.png",
+    description: "Description for Rune Knight.",
+  },
+
+  {
+    name: "Crusader",
+    image: "assets/img/crusader.png",
+    description: "Description for Crusader.",
+  },
+  {
+    name: "Paladin",
+    image: "assets/img/paladin.png",
+    description: "Description for Paladin.",
+  },
+  {
+    name: "Royal Guard",
+    image: "assets/img/royal_guard.png",
+    description: "Description for Royal Guard.",
+  },
+
   {
     name: "Archer",
     image: "assets/img/archer.png",
-    description:
-      "The Archer specializes in ranged attacks using bows and arrows, striking foes from afar with precision.",
+    description: "Description for Archer.",
+  },
+  {
+    name: "Hunter",
+    image: "assets/img/hunter.png",
+    description: "Description for Hunter.",
+  },
+  {
+    name: "Sniper",
+    image: "assets/img/sniper.png",
+    description: "Description for Sniper.",
+  },
+  {
+    name: "Ranger",
+    image: "assets/img/ranger.png",
+    description: "Description for Ranger.",
+  },
+
+  {
+    name: "Bard",
+    image: "assets/img/bard.png",
+    description: "Description for Bard.",
+  },
+  {
+    name: "Clown",
+    image: "assets/img/clown.png",
+    description: "Description for Clown.",
+  },
+  {
+    name: "Minstrel",
+    image: "assets/img/minstrel.png",
+    description: "Description for Minstrel.",
+  },
+
+  {
+    name: "Dancer",
+    image: "assets/img/dancer.png",
+    description: "Description for Dancer.",
+  },
+  {
+    name: "Gypsy",
+    image: "assets/img/gypsy.png",
+    description: "Description for Gypsy.",
+  },
+  {
+    name: "Wanderer",
+    image: "assets/img/wanderer.png",
+    description: "Description for Wanderer.",
+  },
+
+  {
+    name: "Thief",
+    image: "assets/img/thief.png",
+    description: "Description for Thief.",
+  },
+  {
+    name: "Assassin",
+    image: "assets/img/assassin.png",
+    description: "Description for Assassin.",
+  },
+  {
+    name: "Assassin Cross",
+    image: "assets/img/assassin_cross.png",
+    description: "Description for Assassin Cross.",
+  },
+  {
+    name: "Guillotine Cross",
+    image: "assets/img/guillotine_cross.png",
+    description: "Description for Guillotine Cross.",
+  },
+
+  {
+    name: "Rogue",
+    image: "assets/img/rogue.png",
+    description: "Description for Rogue.",
+  },
+  {
+    name: "Stalker",
+    image: "assets/img/stalker.png",
+    description: "Description for Stalker.",
+  },
+  {
+    name: "Shadow Chaser",
+    image: "assets/img/shadow_chaser.png",
+    description: "Description for Shadow Chaser.",
+  },
+
+  {
+    name: "Novice",
+    image: "assets/img/novice.png",
+    description: "Description for Novice.",
+  },
+  {
+    name: "Expanded Super Novice",
+    image: "assets/img/expanded_super_novice.png",
+    description: "Description for Expanded Super Novice.",
+  },
+  {
+    name: "Hyper Novice",
+    image: "assets/img/hyper_novice.png",
+    description: "Description for Hyper Novice.",
   },
 ];
